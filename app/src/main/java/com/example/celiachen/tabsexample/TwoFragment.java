27da56177c7f5ec4f2d6bf14b3aec21f003f73e4
Celia Chen <celiachen0822@gmail.com>
2018-04-18 09:56:26 -0700

    Initial commit

---
+++ app/src/main/java/com/example/celiachen/tabsexample/TwoFragment.java
@@ -0,0 +1,30 @@
+package com.example.celiachen.tabsexample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by celiachen on 4/17/18.
+ */
+
+public class TwoFragment extends Fragment {
+
+    public TwoFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+}
